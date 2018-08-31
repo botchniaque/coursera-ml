b7dac390f69a74e98b9f41ca700085720f93227a
Szymon Bochniak <szymon.bochniak@zalando.de>
2018-08-31 20:55:28 +0200

    find closest centroids

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,10 +21,10 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
-
-
-
+for i = 1:size(X, 1)
+    [_,c] = min(sum((centroids - X(i, :)).^2, 2));
+    idx(i) = c;
+end
 
 
 % =============================================================
